2a6b960dfd670d398558bbcb78b883232ada502c
RuanAlmeida <ruanrodrigues1994@gmail.com>
2017-09-28 14:27:03 -0300

    atualização
    Tela de acompanhamento

---
+++ src/app/acompanhamento/acompanhamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'semdf-acompanhamento',
+  templateUrl: './acompanhamento.component.html',
+  styleUrls: ['./acompanhamento.component.css']
+})
+export class AcompanhamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
